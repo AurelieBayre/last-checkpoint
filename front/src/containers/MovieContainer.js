b3f3f3123a484deaa93b82cd3cc8653c12f7d41c
Aurelie Bayre <a.bayre@gmail.com>
2018-07-25 17:33:27 +0200

    feat: displaying movies from initial state

--- front/src/containers/MovieContainer.js
+++ front/src/containers/MovieContainer.js
@@ -3,16 +3,6 @@ import { connect } from 'react-redux'
 import MoviesList from '../components/MoviesList'
 import { deleteMovie } from '../actions'
 
-const movies = [
-  {
-    id: 0,
-    text: 'Avalon'
-  },
-  {
-    id: 1,
-    text: 'Matrix'
-  },
-]
 
 const mapStateToProps = state => ({
   movies: state
@@ -22,20 +12,8 @@ const mapDispatchToProps = dispatch => ({
   onClickDelete: id => dispatch(deleteMovie(id))
 })
 
-class MovieContainer extends Component {
-
-  render() {
-    return (
-      <div>
-        <h1>Coming soon...the list!</h1>
-        <MoviesList movies={movies} onClickDelete={this.props.onClickDelete}>
-        </MoviesList>
-      </div>
-    )
-  }
-}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(MovieContainer)
+)(MoviesList)
