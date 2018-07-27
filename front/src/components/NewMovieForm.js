ee428a4ae8c926cdb4a545ef464df0e6d36532a6
Aurelie Bayre <a.bayre@gmail.com>
2018-07-27 10:37:55 +0200

    feat: avec reduxForm

--- front/src/components/NewMovieForm.js
+++ front/src/components/NewMovieForm.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Field, reduxForm } from 'redux-form'
+import { Button, Form, FormGroup, Label } from 'reactstrap';
 
-const NewMovieForm = ({ onClickSubmit }) => (
-  <div>
-    <h2>
-      Add a new movie:
-    </h2>
-    <Form>
+const NewMovieForm = ({ onClickSubmit }) => {
+  return (
+    <Form onSubmit={onClickSubmit}>
     <FormGroup>
       <Label for="title">Title</Label>
-      <Input type="title" name="title" id="title" placeholder="title of the movie" />
+      <div>
+        <Field
+          name="title"
+          component="input"
+          type="text"
+          placeholder="nouveau titre"
+        />
+      </div>
     </FormGroup>
-    <Button onClick={() => onClickSubmit('Redux, the Movie...')}>Submit</Button>
+    <Button type="submit">Submit</Button>
   </Form>
-  </div>
-)
+  )
+}
 
+
+NewMovieForm = reduxForm({
+  form: 'simple'
+})(NewMovieForm)
 
 export default NewMovieForm
